b33f7241f701e84f94705cf4d448349a60ec0828
Hoang <githubblackpanter99@gmail.com>
2019-11-12 22:58:53 +0700

    comment

---
+++ resources/views/web/session.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+@include('master.master')
+<body style="background: #eef1f7">
+@include('master.header')
+<div class="container">
+    <div class="chairman-session row">
+        <div class="col-md-2">
+            <img src="{{asset("images/web/profile.png")}}" id="profile" alt="profile">
+        </div>
+        <div class="col-md-6">
+            <ul class="profile-session">
+                <li id="title-session"><i class="fa fa-check"></i>{{$survey[0]->name_survey}}</li>
+                <li id="person-session"><i class="fa fa-edit"></i>{{\Illuminate\Support\Facades\Auth::user()->name}}</li>
+                <li id="des-session"><i class="far fa-sticky-note"></i>{{$survey[0]->description}}</li>
+                <li id="time-session"><i class="fa fa-history"></i>{{$survey[0]->created_at}}</li>
+            </ul>
+        </div>
+    </div>
+    <div class="list-qa-session">
+        <div class="list-qa-survey row">
+            <select name="" id="">
+                <option value="question">Danh sách câu hỏi</option>
+                <option value="survey">Khảo sát ý kiến</option>
+            </select>
+        </div>
+
+        <div class="search-create-qa row">
+            <div class="col-md-3 search-question">
+                <form action="#">
+                    <input type="text" placeholder="search" name="search-qa">
+                    <input type="submit" value="search">
+                </form>
+            </div>
+            <div style="padding: 0px;margin: 0px" class="col-md-6"></div>
+            <div class="col-md-3 create-qa">
+                <button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i> Đặt câu hỏi</button>
+            </div>
+        </div>
+
+        <div class="list-qa" style="margin: 0">
+            @foreach($list_qa as $qa)
+                <div class="single-qa row" style="margin-bottom: 20px">
+                    <img id="img-single-qa" class="img-fluid" src="{{asset("images/web/unanswered-question.png")}}" alt="">
+                    <div class="info-qa">
+                        <ul>
+                            <li id="title-single-qa"><a href="{{route("show_question",[$qa->id_survey,$qa->id_question])}}" style="color:red">{{$qa->title_question}}</a></li>
+                            <li>chưa có câu trả lời</li>
+                        </ul>
+                        <span id="post-by">Đăng bởi: {{$qa->whoposted}}</span>
+                        <span id="amount-comment">Số câu trả lời:1</span>
+                        <span id="amount-like">1 lượt thích</span>
+                    </div>
+
+                </div>
+            @endforeach
+            <div class="single-qa row" style="margin: 0">
+                <img id="img-single-qa" class="img-fluid" src="{{asset("images/web/unanswered-question.png")}}" alt="">
+                <div class="info-qa">
+                    <ul>
+                        <li id="title-single-qa"><a href="#" style="color:red">AAAAA</a></li>
+                        <li>chưa có câu trả lời</li>
+                    </ul>
+                    <span id="post-by">Đăng bởi: N\A</span>
+                    <span id="amount-comment">Số câu trả lời:1</span>
+                    <span id="amount-like">1 lượt thích</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Đặt câu hỏi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{route("add_qa_session",$id)}}" method="post">
+                        @csrf
+                        <input class="form-control" type="text" name="title_question" placeholder="Nội dung câu hỏi..."><br>
+                        <span>Đặt chế độ ẩn danh: </span><input id="toggle-event" type="checkbox" name="post_person" data-size="mini" data-onstyle="danger" data-toggle="toggle"><br>
+                        <input  type="submit" class="btn btn-primary" value="submit">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>
